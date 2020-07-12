1e2c979bf5b8792ebfbf8d8fcb9ee969ef400d2a
Jordan Moss <jmoss636@gmail.com>
2020-07-12 10:15:59 +0100

    adjust db login and env var

--- db/index.js
+++ db/index.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
-mongoose.connect('', {useNewUrlParser: true})
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+
+mongoose.connect(`mongodb://${dbUser}:${dbPassword}@ds052649.mlab.com:52649/shanemock`, {useNewUrlParser: true})
 
 const db = mongoose.connection
 db.on('error', console.error.bind('console', 'connection error:'))
