9f03f1dea1341a907d4c79e54bccc92bb067215b
Jakub Pitucha <vaguevans@gmail.com>
2020-11-26 13:52:09 +0100

    better verification

--- account/verification.js
+++ account/verification.js
@@ -17,4 +17,27 @@ function prepareToken(email, VerifyModel = require('../db/schema/verify')) {
     .catch(console.log)
 }
 
-module.exports = prepareToken
+function verifyUsername(username) {
+    if (username === undefined || username === null) return false
+    const alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwqyz0123456789"
+    username.foreach(function (value) {
+        if (!alphanumeric.contains(value)) return false
+    })
+    return true
+}
+
+function verifyEmail(email) {
+    if (email === undefined || email === null) return false
+    if (!email.contains('@') || !email.contains('.')) return false
+    if (email.indexOf('@') > email.indexOf('.')) return false
+    if (!email.split('@')[0] > 0 || !email.split('@')[1] > 0) return false
+    if (!email.split('.')[0] > 0 || !email.split('.')[1] > 0) return false
+    return true
+}
+
+function verifyPassword(password) {
+    if (password === undefined || password === null) return false
+    return true
+}
+
+module.exports = { prepareToken, verifyUsername, verifyEmail, verifyPassword }
--- routes/local.js
+++ routes/local.js
@@ -3,6 +3,7 @@ const passport = require('./../passport')
 const router = express.Router()
 const User = require('./../db/schema/localUser')
 const Utilities = require('../utils')
+const { verifyUsername, verifyEmail, verifyPassword } = require('./../account/verification')
 
 router.get('/', function(req, res) {
     console.log('main')
@@ -10,11 +11,9 @@ router.get('/', function(req, res) {
 })
 
 router.post('/signup', function(req, res) {
-    if (req.body.username === null || req.body.username === undefined) return res.sendStatus(400)
-    if (req.body.email === null || req.body.email === undefined) return res.sendStatus(400)
-    if (req.body.password === null || req.body.password === undefined) return res.sendStatus(400)
-
-    //NEED MORE PROFESSIONAL VERIFICATION
+    if (!verifyUsername(req.body.username)) return res.sendStatus(400)
+    if (!verifyUsername(req.body.email)) return res.sendStatus(400)
+    if (!verifyUsername(req.body.password)) return res.sendStatus(400)
 
     const salt = Utilities.generateSalt(80)
     const password = Utilities.createSaltedHash(salt, req.body.password)
@@ -31,7 +30,6 @@ router.post('/signup', function(req, res) {
         console.log(newUser)
         res.send('signup')
     })
-    
 })
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
