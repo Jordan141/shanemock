4df708d949298948f63f134d49ea84a5314da00e
Jakub Pitucha <vaguevans@gmail.com>
2020-12-04 18:57:21 +0100

    better err handling

--- passport/strategies/localLogin.js
+++ passport/strategies/localLogin.js
@@ -3,6 +3,7 @@ const localUser = require('../../db/schema/localUser')
 const verifyPassword = require('./../../account/login')
 
 const local = new LocalStrategy((username, password, done) => {
+    if (!username || !password) return done("Invalid credentials")
     localUser.findOne({username})
     .then(user => verifyPassword(password, user))
     .then(result => result ? done(null, result) : done(null, false))
